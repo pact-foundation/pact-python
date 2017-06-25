2076e34390eb80c2d141bdc62379b627fd9881c0
Rory Hart <hartror@gmail.com>
2017-06-25 20:05:24 +1000

    Disabled flake8 F401 for __version__ import

--- pact/__init__.py
+++ pact/__init__.py
@@ -3,6 +3,6 @@ from .consumer import Consumer
 from .matchers import EachLike, SomethingLike, Term
 from .pact import Pact
 from .provider import Provider
-from .__version__ import __version__
+from .__version__ import __version__  # noqa: F401
 
 __all__ = ('Consumer', 'EachLike', 'Pact', 'Provider', 'SomethingLike', 'Term')
