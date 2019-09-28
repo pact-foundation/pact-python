3dc402de3c4506d14a7798c519200763fae16956
Matthew Balvanz <matthew.balvanz@workiva.com>
2019-09-28 15:31:53 -0500

    Set the content type for displaying PyPi documentation

--- setup.py
+++ setup.py
@@ -126,6 +126,7 @@ setup_args = dict(
     description=('Tools for creating and verifying consumer driven contracts'
                  ' using the Pact framework.'),
     long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     author='Matthew Balvanz',
     author_email='matthew.balvanz@workiva.com',
     url='https://github.com/pact-foundation/pact-python',
