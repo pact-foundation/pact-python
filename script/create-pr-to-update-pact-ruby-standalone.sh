410caf1ad89d1bea3658c9d9b1499ec736c4d7cc
Beth Skurrie <beth@bethesque.com>
2020-05-02 09:42:55 +1000

    chore: add script to create a PR to update the pact-ruby-standalone version
    [ci-skip]

---
+++ script/create-pr-to-update-pact-ruby-standalone.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash -e
+
+: "${1?Please supply the pact-ruby-standalone version to upgrade to}"
+
+STANDALONE_VERSION=$1
+TYPE=${2:-feat}
+DASHERISED_VERSION=$(echo "${STANDALONE_VERSION}" | sed 's/\./\-/g')
+BRANCH_NAME="chore/upgrade-to-pact-ruby-standalone-${DASHERISED_VERSION}"
+
+git checkout master
+git checkout setup.py
+git pull origin master
+
+git checkout -b ${BRANCH_NAME}
+
+cat setup.py | sed "s/PACT_STANDALONE_VERSION =.*/PACT_STANDALONE_VERSION = '${STANDALONE_VERSION}'/" > tmp-setup
+mv tmp-setup setup.py
+
+git add setup.py
+git commit -m "${TYPE}: update standalone to ${STANDALONE_VERSION}"
+git push --set-upstream origin ${BRANCH_NAME}
+
+hub pull-request --browse --message "${TYPE}: update standalone to ${STANDALONE_VERSION}"
+
+git checkout master
