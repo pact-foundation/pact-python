81fd6539b62dece980e71463fcf0d3978dc7b734
Elliott Murray <elliottmurray@gmail.com>
2020-08-26 10:16:14 +0100

    chore: release script updates version automaitcally now

--- script/release_prep.sh
+++ script/release_prep.sh
@@ -12,6 +12,10 @@ fi
 TAG_NAME="v$VERSION"
 LAST_TAG=`git describe --abbrev=0`
 
+
+cat pact/__version__.py | sed "s/__version__ = .*/__version__  = '${VERSION}'/" > tmp-version
+mv tmp-version pact/__version__.py
+
 echo "Releasing $TAG_NAME"
 
 echo -e "`git log --pretty=format:'  * %h - %s (%an, %ad)' $LAST_TAG..HEAD`\n$(cat CHANGELOG.md)" > CHANGELOG.md
