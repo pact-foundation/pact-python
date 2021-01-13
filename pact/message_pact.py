fea27c8863c6a5a2a840f50466c9d0bacd21645c
William Infante <william.infante@iress.com>
2021-01-13 15:03:41 +1100

    feat: single message flow

--- pact/message_pact.py
+++ pact/message_pact.py
@@ -2,19 +2,16 @@
 from __future__ import unicode_literals
 
 import fnmatch
+import json
 import os
 import platform
 from subprocess import Popen
 
 import psutil
-import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3 import Retry
 
-from .constants import BROKER_CLIENT_PATH
-from .constants import MOCK_SERVICE_PATH
+
+from .constants import BROKER_CLIENT_PATH, MESSAGE_PATH
 from .matchers import from_term
-
 
 class MessagePact():
     """
@@ -24,11 +21,12 @@ class MessagePact():
     perform tests on a Python consumer. For example:
 
     >>> from pact import Consumer, Provider
-    >>> pact = Consumer('consumer').has_pact_with(Provider('provider'))
-    >>> (pact.given('the echo service is available')
-    ...  .upon_receiving('a request is made to the echo service')
-    ...  .with_request('get', '/echo', query={'text': 'Hello!'})
-    ...  .will_respond_with(200, body='Hello!'))
+    >>> pact = Consumer('MyMessageConsumer').has_pact_with(Provider('provider'))
+    >>> (pact
+    ... .given('Given')
+    ... .expects_to_receive('Expects to Recieve')
+    ... .with_content({'name': 'John', 'document_name': 'sample_document.doc'})
+    ... .with_metadata({'contentType': 'application/json', 'source': 'legacy_api'}))
     >>> with pact:
     ...   requests.get(pact.uri + '/echo?text=Hello!')
 
@@ -38,14 +36,14 @@ class MessagePact():
     does match the defined interaction, it will respond with the text `Hello!`.
     """
 
-    HEADERS = {'X-Pact-Mock-Service': 'true'} ##
+    HEADERS = {'X-Pact-Mock-Service': 'true'}
 
     MANDATORY_FIELDS = {'provider_state', 'description', 'metadata', 'content'}
 
     def __init__(self, consumer, provider, log_dir=None,
                     publish_to_broker=False, broker_base_url=None,
                     broker_username=None, broker_password=None, broker_token=None,
-                    pact_dir=None, version='3.0.0', file_write_mode='overwrite'):
+                    pact_dir=None, version='3.0.0', file_write_mode='merge'):
         """
         Create a Pact instance.
 
@@ -104,7 +102,8 @@ class MessagePact():
         self.publish_to_broker = publish_to_broker
         self.version = version
         self._process = None
-        self._messages = []
+        self._messages = [] # single message only
+        self._message_process = None
 
     def given(self, provider_state):
         """
@@ -132,178 +131,78 @@ class MessagePact():
         self._messages[0]['content'] = content
         return self
 
-
-
     def expects_to_receive(self, description):
         self._insert_message_if_complete()
         self._messages[0]['description'] = description
         return self 
 
-
     @staticmethod
     def _normalize_consumer_name(name):
         return name.lower().replace(' ', '_')
 
-    def publish(self): # TODO: common
-        """Publish the generated pact files to the specified pact broker."""
-        if self.broker_base_url is None \
-                and "PACT_BROKER_BASE_URL" not in os.environ:
-            raise RuntimeError("No pact broker URL specified. "
-                               + "Did you expect the PACT_BROKER_BASE_URL "
-                               + "environment variable to be set?")
+    # def publish(self): # TODO: add pact file generation
+    #     """Publish the generated pact files to the specified pact broker."""
+    #     if self.broker_base_url is None \
+    #             and "PACT_BROKER_BASE_URL" not in os.environ:
+    #         raise RuntimeError("No pact broker URL specified. "
+    #                            + "Did you expect the PACT_BROKER_BASE_URL "
+    #                            + "environment variable to be set?")
 
-        pact_files = fnmatch.filter(
-            os.listdir(self.pact_dir),
-            self._normalize_consumer_name(self.consumer.name) + '*.json'
-        )
-        command = [
-            BROKER_CLIENT_PATH,
-            'publish',
-            '--consumer-app-version={}'.format(self.consumer.version)]
+    #     pact_files = fnmatch.filter(
+    #         os.listdir(self.pact_dir),
+    #         self._normalize_consumer_name(self.consumer.name) + '*.json'
+    #     )
+    #     command = [
+    #         BROKER_CLIENT_PATH,
+    #         'publish',
+    #         '--consumer-app-version={}'.format(self.consumer.version)]
 
-        if self.broker_base_url is not None:
-            command.append('--broker-base-url={}'.format(self.broker_base_url))
-        if self.broker_username is not None:
-            command.append('--broker-username={}'.format(self.broker_username))
-        if self.broker_password is not None:
-            command.append('--broker-password={}'.format(self.broker_password))
-        if self.broker_token is not None:
-            command.append('--broker-token={}'.format(self.broker_token))
+    #     if self.broker_base_url is not None:
+    #         command.append('--broker-base-url={}'.format(self.broker_base_url))
+    #     if self.broker_username is not None:
+    #         command.append('--broker-username={}'.format(self.broker_username))
+    #     if self.broker_password is not None:
+    #         command.append('--broker-password={}'.format(self.broker_password))
+    #     if self.broker_token is not None:
+    #         command.append('--broker-token={}'.format(self.broker_token))
 
-        command.extend(pact_files)
+    #     command.extend(pact_files)
 
-        if self.consumer.tag_with_git_branch:
-            command.append('--tag-with-git-branch')
+    #     if self.consumer.tag_with_git_branch:
+    #         command.append('--tag-with-git-branch')
 
-        if self.consumer.tags is not None:
-            for tag in self.consumer.tags:
-                command.extend(['-t', tag])
+    #     if self.consumer.tags is not None:
+    #         for tag in self.consumer.tags:
+    #             command.extend(['-t', tag])
 
-        publish_process = Popen(command)
-        publish_process.wait()
-        if publish_process.returncode != 0:
-            url = self.broker_base_url or os.environ["PACT_BROKER_BASE_URL"]
-            raise RuntimeError(
-                "There was an error while publishing to the "
-                + "pact broker at {}."
-                .format(url))
-
-    # def setup(self):
-    #     pass
-        # """Configure the Mock Service to ready it for a test."""
-        # try:
-        #     resp = requests.delete(
-        #         self.uri + '/interactions', headers=self.HEADERS, verify=False)
-
-        #     assert resp.status_code == 200, resp.text
-        #     resp = requests.put(
-        #         self.uri + '/interactions',
-        #         headers=self.HEADERS,
-        #         verify=False,
-        #         json={"interactions": self._interactions})
-
-        #     assert resp.status_code == 200, resp.text
-        # except AssertionError:
-        #     raise
-
-    def start_service(self):
-        """
-        Start the external Mock Service.
-
-        :raises RuntimeError: if there is a problem starting the mock service.
-        """
-        command = [
-            MOCK_SERVICE_PATH,
-            'service',
-            '--host={}'.format(self.host_name),
-            '--port={}'.format(self.port),
-            '--log', '{}/pact-mock-service.log'.format(self.log_dir),
-            '--pact-dir', self.pact_dir,
-            '--pact-file-write-mode', self.file_write_mode,
-            '--pact-specification-version={}'.format(self.version),
-            '--consumer', self.consumer.name,
-            '--provider', self.provider.name]
-
-        if self.ssl:
-            command.append('--ssl')
-        if self.sslcert:
-            command.extend(['--sslcert', self.sslcert])
-        if self.sslkey:
-            command.extend(['--sslkey', self.sslkey])
-
-        self._process = Popen(command)
-        self._wait_for_server_start()
-
-    def stop_service(self):
-        """Stop the external Mock Service."""
-        is_windows = 'windows' in platform.platform().lower()
-        if is_windows:
-            # Send the signal to ruby.exe, not the *.bat process
-            p = psutil.Process(self._process.pid)
-            for child in p.children(recursive=True):
-                child.terminate()
-            p.wait()
-            if psutil.pid_exists(self._process.pid):
-                raise RuntimeError(
-                    'There was an error when stopping the Pact mock service.')
-
-        else:
-            self._process.terminate()
-
-            self._process.communicate()
-            if self._process.returncode != 0:
-                raise RuntimeError(
-                    'There was an error when stopping the Pact mock service.')
-        if (self.publish_to_broker):
-            self.publish()
-
-    def upon_receiving(self, scenario):
-        """
-        Define the name of this contract.
-
-        :param scenario: A unique name for this contract.
-        :type scenario: basestring
-        :rtype: Pact
-        """
-        self._insert_interaction_if_complete()
-        self._interactions[0]['description'] = scenario
-        return self
-
-    def verify(self): # TODO: common
-        """
-        Have the mock service verify all interactions occurred.
-
-        Calls the mock service to verify that all interactions occurred as
-        expected, and has it write out the contracts to disk.
-
-        :raises AssertionError: When not all interactions are found.
-        """
-        pass
-        # self._interactions = []
-        # resp = requests.get(
-        #     self.uri + '/interactions/verification',
-        #     headers=self.HEADERS, verify=False)
-        # assert resp.status_code == 200, resp.text
-        # resp = requests.post(
-        #     self.uri + '/pact', headers=self.HEADERS, verify=False)
-        # assert resp.status_code == 200, resp.text
-
+    #     publish_process = Popen(command)
+    #     publish_process.wait()
+    #     if publish_process.returncode != 0:
+    #         url = self.broker_base_url or os.environ["PACT_BROKER_BASE_URL"]
+    #         raise RuntimeError(
+    #             "There was an error while publishing to the "
+    #             + "pact broker at {}."
+    #             .format(url))
 
     def write_to_pact_file(self):
 
-        for x in self._message_interactions:
-            command = [
-                MESSAGE_PATH,
-                'update',
-                json.dumps(x._messages[0]),
-                '--pact-dir', self.pact_dir,
-                '--pact-specification-version={}'.format(self.version),
-                '--consumer', self.consumer.name + "_message",
-                '--provider', self.provider.name + "_message"]
+        # for x in self._message_interactions:
+        # temporarily assumed we only have a single message
+        command = [
+            MESSAGE_PATH,
+            'update',
+            json.dumps(self._messages[0]),
+            '--pact-dir', self.pact_dir,
+            '--pact-specification-version={}'.format(self.version),
+            '--consumer', self.consumer.name + "_message",
+            '--provider', self.provider.name + "_message"]
 
-            print("********* command: {}".format(command))
+        self._message_process = Popen(command)
 
-            self._message_process = Popen(command)
+    # def add_message(self):
+    #     message = Message()
+    #     self._message_interactions.append(message)
+    #     return message
 
     def _insert_message_if_complete(self):
         """
@@ -318,20 +217,11 @@ class MessagePact():
                  for field in self.MANDATORY_FIELDS):
             self._messages.insert(0, {})
 
-
-    # def __enter__(self):
-    #     """
-    #     Enter a Python context.
-
-    #     Sets up the mock service to expect the client requests.
-    #     """
-    #     self.setup()
-
     def __exit__(self, exc_type, exc_val, exc_tb):
         """
         Exit a Python context.
 
-        Calls the mock service to verify that all interactions occurred as
+        Calls the mock service to verify the message occurred as
         expected, and has it write out the contracts to disk.
         """
         if (exc_type, exc_val, exc_tb) != (None, None, None):
@@ -340,79 +230,3 @@ class MessagePact():
         self.write_to_pact_file()
 
 
-class FromTerms(object):
-    """Base class for objects built from a collection of Matchers."""
-
-    def json(self):
-        """Convert the object to a JSON version of the mock service."""
-        raise NotImplementedError
-
-
-class Request(FromTerms):
-    """Represents an HTTP request and supports Matchers on its properties."""
-
-    def __init__(self, method, path, body=None, headers=None, query=''):
-        """
-        Create a new instance of Request.
-
-        :param method: The HTTP method that is expected.
-        :type method: str
-        :param path: The URI path that is expected on this request.
-        :type path: str, Matcher
-        :param body: The contents of the body of the expected request.
-        :type body: str, dict, list
-        :param headers: The headers of the expected request.
-        :type headers: dict
-        :param query: The URI query of the expected request.
-        :type query: str or dict
-        """
-        self.method = method
-        self.path = from_term(path)
-        self.body = from_term(body)
-        self.headers = from_term(headers)
-        self.query = from_term(query)
-
-    def json(self):
-        """Convert the Request to a JSON version for the mock service."""
-        request = {'method': self.method, 'path': self.path}
-
-        if self.headers:
-            request['headers'] = self.headers
-
-        if self.body is not None:
-            request['body'] = self.body
-
-        if self.query:
-            request['query'] = self.query
-
-        return request
-
-
-class Response(FromTerms):
-    """Represents an HTTP response and supports Matchers on its properties."""
-
-    def __init__(self, status, headers=None, body=None):
-        """
-        Create a new Response.
-
-        :param status: The expected HTTP status of the response.
-        :type status: int
-        :param headers: The expected headers of the response.
-        :type headers: dict
-        :param body: The expected body of the response.
-        :type body: str, dict, or list
-        """
-        self.status = status
-        self.body = from_term(body)
-        self.headers = from_term(headers)
-
-    def json(self):
-        """Convert the Response to a JSON version for the mock service."""
-        response = {'status': self.status}
-        if self.body is not None:
-            response['body'] = self.body
-
-        if self.headers:
-            response['headers'] = self.headers
-
-        return response
