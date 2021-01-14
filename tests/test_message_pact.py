299046f4333c8a1706ecf889e8888cea8f121939
Tuan Pham <q.tuan.p@gmail.com>
2021-01-14 14:26:42 +1100

    chore: add generate_pact_test

--- tests/test_message_pact.py
+++ tests/test_message_pact.py
@@ -1,9 +1,13 @@
 import os
+from subprocess import Popen
 from unittest import TestCase
+
+from mock import patch, call, Mock
 
 from pact.message_consumer import MessageConsumer, Provider
 from pact.message_pact import MessagePact
-
+from pact.constants import MESSAGE_PATH
+from pact import pact as pact
 
 class MessagePactTestCase(TestCase):
     def setUp(self):
@@ -61,19 +65,19 @@ class MessagePactTestCase(TestCase):
         self.assertEqual(len(target._messages), 1)
 
         self.assertEqual(
-            target._messages[0]['provider_state'],
-            'there is an alligator named John')
+            target._messages[0]['providerStates'],
+            [{'name':'there is an alligator named John'}])
 
         self.assertEqual(
             target._messages[0]['description'],
             'an alligator message')
 
         self.assertEqual(
-            target._messages[0]['content'],
+            target._messages[0]['contents'],
             {'name': 'John', 'document_name': 'sample_document.doc'})
 
         self.assertEqual(
-            target._messages[0]['metadata'],
+            target._messages[0]['metaData'],
             {'contentType': 'application/json', 'source': 'legacy_api'})
 
     def test_definition_without_given(self):
@@ -87,18 +91,54 @@ class MessagePactTestCase(TestCase):
 
         self.assertEqual(len(target._messages), 1)
 
-        self.assertIsNone(target._messages[0].get('provider_state'))
+        self.assertIsNone(target._messages[0].get('providerStates'))
 
         self.assertEqual(
             target._messages[0]['description'],
             'an crocodile message')
 
         self.assertEqual(
-            target._messages[0]['content'],
+            target._messages[0]['contents'],
             {'name': 'Mary'})
 
         self.assertEqual(
-            target._messages[0]['metadata'],
+            target._messages[0]['metaData'],
             {'source': 'legacy_api'})
 
-    # multiple interactions / messages not currently supported
+
+class PactGeneratePactFileTestCase(TestCase):
+    def setUp(self):
+        # super(PactGeneratePactFileTestCase, self).setUp()
+        pass
+
+    def test_call_pact_message_to_generate_pact_file(self):
+        self.addCleanup(patch.stopall)
+        self.mock_Popen = patch.object(pact, 'Popen', autospec=True).start()
+        self.mock_Popen.return_value.returncode = 0
+
+        self.consumer = MessageConsumer('TestConsumer')
+        self.provider = Provider('TestProvider')
+
+        target = MessagePact(
+            self.consumer, self.provider, log_dir='/logs', pact_dir='/pacts',
+            version='3.0.0', file_write_mode='merge')
+
+        (target
+            .given('There is an alligator named John')
+            .expects_to_receive('an alligator message')
+            .with_content({
+                'name': 'John', 
+                'document_name': 'sample_document.doc'
+            }).with_metadata({
+                'contentType': 'application/json',
+                'source': 'legacy_api'}))
+
+        target.write_to_pact_file()
+
+        self.mock_Popen.assert_called_once_with([
+            MESSAGE_PATH, 'update',
+            '--consumer', 'TestConsumer_message',
+            '--provider', 'TestProvider_message',
+            '--consumer-app-version=3.0.0',
+            '--pact-dir', '/pacts'
+            ])
