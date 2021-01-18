19827d002854a51a67c420659cec8272bae32353
Tuan Pham <q.tuan.p@gmail.com>
2021-01-18 16:06:42 +1100

    chore: remove test param for provider

--- examples/message/tests/conftest.py
+++ examples/message/tests/conftest.py
@@ -4,8 +4,3 @@ def pytest_addoption(parser):
         "--publish-pact", type=str, action="store",
         help="Upload generated pact file to pact broker with version"
     )
-
-    parser.addoption(
-        "--provider-url", type=str, action="store",
-        help="The url to our provider."
-    )
