5448d8c0a9b2f3d613827a958cc2d8515eff24e0
William Infante <williaminfante@gmail.com>
2021-01-20 21:07:39 +1100

    test: remove mock and check generated json file

--- examples/message/tests/consumer/test_message_consumer.py
+++ examples/message/tests/consumer/test_message_consumer.py
@@ -1,11 +1,13 @@
 """pact test for user service client"""
 
 import logging
-from unittest.mock import patch
-
 import pytest
-from pact import MessageConsumer, Provider
+import time
 
+from os import remove
+from os.path import isfile
+
+from pact import MessageConsumer, Provider
 from src.message_handler import MessageHandler, CustomError
 
 log = logging.getLogger(__name__)
@@ -17,21 +19,35 @@ PACT_BROKER_USERNAME = "pactbroker"
 PACT_BROKER_PASSWORD = "pactbroker"
 PACT_DIR = 'pacts'
 
+consumer_name = 'DetectContentLambda'
+provider_name = 'ContentProvider'
+expected_json = (f"{consumer_name.lower().replace(' ', '_')}_message-"
+                 + f"{provider_name.lower().replace(' ', '_')}_message.json")
 
 @pytest.fixture(scope='session')
 def pact(request):
     version = request.config.getoption('--publish-pact')
     publish = True if version else False
 
-    pact = MessageConsumer('DetectContentLambda', version=version).has_pact_with(
-        Provider('ContentProvider'),
+    pact = MessageConsumer(consumer_name, version=version).has_pact_with(
+        Provider(provider_name),
         pact_dir=PACT_DIR, publish_to_broker=publish, broker_base_url=PACT_BROKER_URL,
         broker_username=PACT_BROKER_USERNAME, broker_password=PACT_BROKER_PASSWORD)
 
     yield pact
 
 
+def cleanup_json(expected_json):
+    """
+    Remove existing json file before test if any
+    """
+    if (isfile(f"pacts/{expected_json}")):
+        remove(f"pacts/{expected_json}")
+
+
 def test_generate_pact_file(pact):
+    cleanup_json(expected_json)
+
     expected_event = {
         'documentName': 'document.doc',
         'creator': 'TP',
@@ -46,14 +62,17 @@ def test_generate_pact_file(pact):
          'Content-Type': 'application/json'
      }))
 
-    with patch.object(pact, 'write_to_pact_file') as mock:
-        with pact:
-            # handler needs 'documentType' == 'microsoft-word'
-            MessageHandler(expected_event)
-        mock.assert_called_once()
+    with pact:
+        # handler needs 'documentType' == 'microsoft-word'
+        MessageHandler(expected_event)
+
+    time.sleep(1)
+    assert isfile(f"pacts/{expected_json}") == 1
 
 
 def test_throw_exception_handler(pact):
+    cleanup_json(expected_json)
+
     wrong_event = {
         'documentName': 'spreadsheet.xls',
         'creator': 'WI',
@@ -69,8 +88,9 @@ def test_throw_exception_handler(pact):
      }))
 
     with pytest.raises(CustomError):
-        with patch.object(pact, 'write_to_pact_file') as mock:
-            with pact:
-                # handler needs 'documentType' == 'microsoft-word'
-                MessageHandler(wrong_event)
-            mock.assert_not_called()
+        with pact:
+            # handler needs 'documentType' == 'microsoft-word'
+            MessageHandler(wrong_event)
+
+    time.sleep(1)
+    assert isfile(f"pacts/{expected_json}") == 0
