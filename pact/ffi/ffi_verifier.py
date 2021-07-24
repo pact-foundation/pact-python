4b239e4dc3358b0981c6b5b759a22158d94dbb28
Elliott Murray <elliottmurray@gmail.com>
2021-07-24 10:36:28 +0100

    chore: move dyn libs to libs folder@

--- pact/ffi/ffi_verifier.py
+++ pact/ffi/ffi_verifier.py
@@ -20,11 +20,11 @@ class FFIVerify(object):
         target_platform = platform.platform().lower()
 
         if 'darwin' in target_platform or 'macos' in target_platform:
-            libname = "libpact_ffi-osx-x86_64.dylib"
+            libname = "libs/libpact_ffi-osx-x86_64.dylib"
         elif 'linux' in target_platform:
-            libname = "./libpact_ffi-linux-x86_64.so"
+            libname = "libs/libpact_ffi-linux-x86_64.so"
         elif 'windows' in target_platform:
-            libname = "libpact_ffi-osx-x86_64.dylib"
+            libname = "libs/libpact_ffi-osx-x86_64.dylib"
         else:
             msg = ('Unfortunately, {} is not a supported platform. Only Linux,'
                    ' Windows, and OSX are currently supported.').format(
