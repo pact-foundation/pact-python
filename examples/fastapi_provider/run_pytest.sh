4f9ecf35595fe060d3754de4a2100132e48cf7c0
Mike Geeves <mike.geeves@gmail.com>
2022-01-21 23:19:34 +0000

    docs(fastapi example): wait for the server to be ready

--- examples/fastapi_provider/run_pytest.sh
+++ examples/fastapi_provider/run_pytest.sh
@@ -3,4 +3,4 @@ set -o pipefail
 
 # Unlike in the Flask example, here the FastAPI service is started up as a pytest fixture. This is then including the
 # main and pact routes via fastapi_provider.py to run the tests against
-pytest --run-broker True --publish-pact 1
+pytest -rP --run-broker True --publish-pact 1
