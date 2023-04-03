429e17116daa9bed752a4b3f31860857be56c3c1
Mike Geeves <mike.geeves@bearly-compiling.com>
2023-04-03 09:01:37 +0100

    build: use a single Dockerfile, providing args for the Python version instead of multiple files

--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
-set -eo pipefail
+set -euo pipefail
 
-if [ $# -ne 1 ]; then
-    echo "$0: usage: build.sh 37|38|39|310|311"
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 PYTHON_VERSION [ALPINE_VERSION]"
+    echo
+    echo "Example:"
+    echo "$0 3.11       Build using Python 3.11, default Alpine"
+    echo "$0 3.6 3.16   Build using Python 3.6, Alpine 3.15"
     exit 1
 fi
-DOCKER_ENV=$1
-echo "Building env ${DOCKER_ENV}"
 
-DOCKER_IMAGE="pactfoundation:python${DOCKER_ENV}"
-DOCKER_FILE="docker/py${DOCKER_ENV}.Dockerfile"
+PY=$1
+ALPINE=${2:-3.17}
+echo "Building env for Python: ${PY}, Alpine: ${ALPINE}"
 
-docker build -t "$DOCKER_IMAGE" -f "$DOCKER_FILE" .
+DOCKER_IMAGE="pactfoundation:python${PY}"
+DOCKER_FILE="docker/Dockerfile"
+
+docker build \
+    --build-arg PY="$PY" \
+    --build-arg TOXPY="$(sed 's/\.//' <<< "$PY")" \
+    --build-arg ALPINE="${ALPINE}" \
+    -t "$DOCKER_IMAGE" -f "$DOCKER_FILE" .
+
+echo
+echo "Image successfully built and tagged as: ${DOCKER_IMAGE}"
