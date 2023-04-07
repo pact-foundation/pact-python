d1a4708d4fcd4609e82065ba5868239e819c15e4
Gaurav Bhardwaj <Gaurav_Bhardwaj@mckinsey.com>
2023-04-07 13:13:07 +0530

    Removed Dependency for simplejson

---
+++ pact/decimal_encoder.py
@@ -0,0 +1,8 @@
+import json
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
