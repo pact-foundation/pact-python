c1719da0c0610e5b28b0f54e4faf35757c24a560
Yousaf Nabi <you@saf.dev>
2023-05-05 10:17:55 +0100

    Revert "ci: test arm64 on cirrus-ci / test win/osx on gh"
    This reverts commit 28440da2e1f6598b3d61a08c41f8382cf4bf4bf9.

--- run-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for arch in arm64 amd64; do
-    # for version in 3.6; do 
-    for version in 3.7 3.8 3.9 3.10 3.11; do
-        docker build -t python-$arch-$version --build-arg PYTHON_VERSION=$version --platform=linux/$arch .
-        docker run -it --rm python-$arch-$version
-    done
-done
