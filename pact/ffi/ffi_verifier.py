440aacdb98a5a73d242408367e0b877beb004d3e
Yousaf Nabi <you@saf.dev>
2023-06-17 09:37:19 +0100

    chore(ffi): initita ffi verifier progress

--- pact/ffi/ffi_verifier.py
+++ pact/ffi/ffi_verifier.py
@@ -20,8 +20,35 @@ class FFIVerify(object):
 
         ffi = FFI()
         lib = RegisterFfi().get_ffi_lib(ffi)
-        result = lib.pactffi_version()
-        return ffi.string(result).decode('utf-8')
+        lib.pactffi_log_to_stdout(5)
+        verifier = lib.pactffi_verifier_new_for_application(b'pact-python',b'1.0.0')
+        lib.pactffi_verifier_set_provider_info(verifier,provider.encode('ascii'),b'http', b'localhost', 8000, b'/');
+        result = lib.pactffi_verifier_execute(verifier);
+        get_logs = lib.pactffi_verifier_logs(verifier);
+        
+        if pacts:
+            for pact in pacts:
+                lib.pactffi_verifier_add_file_source(verifier, pact.encode('ascii'));
+        
+
+        
+        
+        # lib.pactffi_log_to_stderr(5)
+        # lib.pactffi_log_to_buffer(5)
+        # result = lib.pactffi_version()
+        # foo = [tuple(map(lambda x: x.encode('ascii'), tup)) for tup in pacts]
+        # lib.pactffi_verifier_add_directory_source(verifier, *pacts.each.encode('ascii'));
+
+        
+
+        # lib.pactffi_verifier_add_file_source(verifier, b'./pacts/consumer-provider.json');
+
+
+
+        lib.pactffi_verifier_shutdown(verifier);
+        # return get_logs
+        print(ffi.string(get_logs).decode('utf-8'))
+        return result
 
     def _validate_input(self, pacts, **kwargs):
         if len(pacts) == 0 and not self._broker_present(**kwargs):
--- tests/ffi/test_ffi_verifier.py
+++ tests/ffi/test_ffi_verifier.py
@@ -17,3 +17,18 @@ def test_pact_urls_or_broker_required(mock_Popen):
         verifier.verify(provider='provider', provider_base_url='http://localhost')
 
     assert 'Pact urls or Pact broker required' in str(e)
+
+def test_pact_files_provided():
+    # mock_Popen.return_value.returncode = 0
+    wrapper = FFIVerify()
+
+    result = wrapper.verify(
+                                    './pacts/consumer-provider.json',
+        #                             './pacts/consumer-provider2.json',
+                                    provider='test_provider',
+                                    provider_base_url='http://localhost')
+    assert 'foo' in str(result)
+    # self.default_call.insert(0, './pacts/consumer-provider.json')
+    # self.default_call.insert(1, './pacts/consumer-provider2.json')
+    # self.assertProcess(*self.default_call)
+    # self.assertEqual(result, 0)
