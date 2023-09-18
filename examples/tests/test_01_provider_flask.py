e2103f78c9d70bb45e4ae559ed88745669a354ee
JP-Ellis <josh@jpellis.me>
2023-09-18 15:46:38 +1000

    chore(example): migrate flask provider example
    Following the changes to the FastAPI example, this migrates the Flask
    provider example to the new structure. The example relies on the
    consumer having published contracts, and the flask provider is verified
    against those contracts.
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

---
+++ examples/tests/test_01_provider_flask.py
@@ -0,0 +1,136 @@
+"""
+Test the Flask provider with Pact.
+
+This module tests the Flask provider defined in `src/flask.py` against the mock
+consumer. The mock consumer is set up by Pact and will replay the requests
+defined by the consumers. Pact will then validate that the provider responds
+with the expected responses.
+
+The provider will be expected to be in a given state in order to respond to
+certain requests. For example, when fetching a user's information, the provider
+will need to have a user with the given ID in the database. In order to avoid
+side effects, the provider's database calls are mocked out using functionalities
+from `unittest.mock`.
+
+In order to set the provider into the correct state, this test module defines an
+additional endpoint on the provider, in this case `/_pact/provider_states`.
+Calls to this endpoint mock the relevant database calls to set the provider into
+the correct state.
+"""
+
+
+from __future__ import annotations
+
+from multiprocessing import Process
+from typing import Any, Generator
+from unittest.mock import MagicMock
+
+import pytest
+from flask import request
+from pact import Verifier
+from yarl import URL
+
+from src.flask import app
+
+PROVIDER_URL = URL("http://localhost:8080")
+
+
+@app.route("/_pact/provider_states", methods=["POST"])
+async def mock_pact_provider_states() -> dict[str, str | None]:
+    """
+    Define the provider state.
+
+    For Pact to be able to correctly tests compliance with the contract, the
+    internal state of the provider needs to be set up correctly. Naively, this
+    would be achieved by setting up the database with the correct data for the
+    test, but this can be slow and error-prone. Instead this is best achieved by
+    mocking the relevant calls to the database so as to avoid any side effects.
+
+    For Pact to be able to correctly get the provider into the correct state,
+    this function is used to define an additional endpoint on the provider. This
+    endpoint is called by Pact before each test to ensure that the provider is
+    in the correct state.
+    """
+    mapping = {
+        "user 123 doesn't exist": mock_user_123_doesnt_exist,
+        "user 123 exists": mock_user_123_exists,
+    }
+    return {"result": mapping[request.json["state"]]()}
+
+
+def run_server() -> None:
+    """
+    Run the Flask server.
+
+    This function is required to run the Flask server in a separate process. A
+    lambda cannot be used as the target of a `multiprocessing.Process` as it
+    cannot be pickled.
+    """
+    app.run(host=PROVIDER_URL.host, port=PROVIDER_URL.port)
+
+
+@pytest.fixture(scope="module")
+def verifier() -> Generator[Verifier, Any, None]:
+    """Set up the Pact verifier."""
+    proc = Process(target=run_server, daemon=True)
+    verifier = Verifier(
+        provider="UserProvider",
+        provider_base_url=str(PROVIDER_URL),
+    )
+    proc.start()
+    yield verifier
+    proc.kill()
+
+
+def mock_user_123_doesnt_exist() -> None:
+    """Mock the database for the user 123 doesn't exist state."""
+    import src.flask
+
+    src.flask.FAKE_DB = MagicMock()
+    src.flask.FAKE_DB.get.return_value = None
+
+
+def mock_user_123_exists() -> None:
+    """
+    Mock the database for the user 123 exists state.
+
+    You may notice that the return value here differs from the consumer's
+    expected response. This is because the consumer's expected response is
+    guided by what the consumer users.
+
+    By using consumer-driven contracts and testing the provider against the
+    consumer's contract, we can ensure that the provider is only providing what
+    """
+    import src.flask
+
+    src.flask.FAKE_DB = MagicMock()
+    src.flask.FAKE_DB.get.return_value = {
+        "id": 123,
+        "name": "Verna Hampton",
+        "created_on": "2016-12-15T20:16:01",
+        "ip_address": "10.1.2.3",
+        "hobbies": ["hiking", "swimming"],
+        "admin": False,
+    }
+
+
+def test_against_broker(broker: URL, verifier: Verifier) -> None:
+    """
+    Test the provider against the broker.
+
+    The broker will be used to retrieve the contract, and the provider will be
+    tested against the contract.
+
+    As Pact is a consumer-driven, the provider is tested against the contract
+    defined by the consumer. The consumer defines the expected request to and
+    response from the provider.
+
+    For an example of the consumer's contract, see the consumer's tests.
+    """
+    code, _ = verifier.verify_with_broker(
+        broker_url=str(broker),
+        published_verification_results=True,
+        provider_states_setup_url=str(PROVIDER_URL / "_pact" / "provider_states"),
+    )
+
+    assert code == 0
