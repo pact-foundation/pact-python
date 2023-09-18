4da4d14c524accccae40539a8164265d874ac054
JP-Ellis <josh@jpellis.me>
2023-09-18 14:20:39 +1000

    chore(example): migrate fastapi provider example
    This migrates the Pact FastAPI provider from the old standalone
    examples, and merges it with the new combined examples.
    
    The consumer tests are executed first which publishes the contracts with
    the broker. The provider test is then executed against the broker to
    verify compliance with the published contracts.
    
    This does, at this stage, create an unusual interdependence between
    tests which typically should be avoided. I plan to fix this at a later
    stage.
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- examples/fastapi_provider/run_pytest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o pipefail
-
-# Unlike in the Flask example, here the FastAPI service is started up as a pytest fixture. This is then including the
-# main and pact routes via fastapi_provider.py to run the tests against
-pytest --run-broker True --publish-pact 1\
