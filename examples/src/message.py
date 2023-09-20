e2b6389b50b72f4602301ff10797df18433ab362
JP-Ellis <josh@jpellis.me>
2023-09-20 13:47:26 +1000

    chore(example): migrate message pact example
    Migrate the old isolated example and combine the test with the other
    examples so that they can all be run at once.
    
    Massive thanks to @YOU54F for identifying the switch up between the
    `given` and `expected`!
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

---
+++ examples/src/message.py
@@ -0,0 +1,96 @@
+"""
+Handler for non-HTTP interactions.
+
+This module implements a very basic handler to handle JSON payloads which might
+be sent from Kafka, or some queueing system. Unlike a HTTP interaction, the
+handler is solely responsible for processing the message, and does not
+necessarily need to send a response.
+"""
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any
+
+
+class Filesystem:
+    """Filesystem interface."""
+
+    def __init__(self) -> None:
+        """Initialize the filesystem connection."""
+
+    def write(self, _file: str, _contents: str) -> None:
+        """Write contents to a file."""
+        raise NotImplementedError
+
+    def read(self, file: str) -> str:
+        """Read contents from a file."""
+        raise NotImplementedError
+
+
+class Handler:
+    """
+    Message queue handler.
+
+    This class is responsible for handling messages from the queue.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize the handler.
+
+        This ensures the underlying filesystem is ready to be used.
+        """
+        self.fs = Filesystem()
+
+    def process(self, event: dict[str, Any]) -> str | None:
+        """
+        Process an event from the queue.
+
+        The event is expected to be a dictionary with the following mandatory
+        keys:
+
+        - `action`: The action to be performed, either `READ` or `WRITE`.
+        - `path`: The path to the file to be read or written.
+
+        The event may also contain an optional `contents` key, which is the
+        contents to be written to the file. If the `contents` key is not
+        present, an empty file will be written.
+        """
+        self.validate_event(event)
+
+        if event["action"] == "WRITE":
+            return self.fs.write(event["path"], event.get("contents", ""))
+        if event["action"] == "READ":
+            return self.fs.read(event["path"])
+
+        msg = "Invalid action."
+        raise ValueError(msg)
+
+    @staticmethod
+    def validate_event(event: dict[str, Any] | Any) -> None:  # noqa: ANN401
+        """
+        Validates the event received from the queue.
+
+        The event is expected to be a dictionary with the following mandatory
+        keys:
+
+        - `action`: The action to be performed, either `READ` or `WRITe`.
+        - `path`: The path to the file to be read or written.
+        """
+        if not isinstance(event, dict):
+            msg = "Event must be a dictionary."
+            raise TypeError(msg)
+        if "action" not in event:
+            msg = "Event must contain an 'action' key."
+            raise ValueError(msg)
+        if "path" not in event:
+            msg = "Event must contain a 'path' key."
+            raise ValueError(msg)
+        if event["action"] not in ["READ", "WRITE"]:
+            msg = "Event must contain a valid 'action' key."
+            raise ValueError(msg)
+        try:
+            Path(event["path"])
+        except TypeError as err:
+            msg = "Event must contain a valid 'path' key."
+            raise ValueError(msg) from err
