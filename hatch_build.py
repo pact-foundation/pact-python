6b8009fb0e179a3bca505b56a746c9e47770a845
JP-Ellis <josh@jpellis.me>
2023-10-06 14:53:13 +1100

    fix(build): include omitted `lib` dir
    When packaging the Ruby Pact binaries, I initially removed the `lib` dir
    naively believing that the Pact binaries were static. This is in fact
    incorrect, and I adjusted the extraction to extract _all_ of the
    content (and only remove the README.md).
    
    The unit tests all passed which affirmed my initial belief.
    Unfortunately (as I have now discovered), the unit tests mock out the
    call to the binaries, and therefore the test suite did not actuall test
    the execution of the binaries.
    
    This commit also incorporate a few other changes to the build script
    and/or pyproject.toml configuration from the `master` branch.
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- hatch_build.py
+++ hatch_build.py
@@ -1,33 +1,64 @@
-"""Hatchling build hook for Pact binary download."""
+"""
+Hatchling build hook for binary downloads.
+
+Pact Python is built on top of the Ruby Pact binaries and the Rust Pact library.
+This build script downloads the binaries and library for the current platform
+and installs them in the `pact` directory under `/bin` and `/lib`.
+
+The version of the binaries and library can be controlled with the
+`PACT_BIN_VERSION` and `PACT_LIB_VERSION` environment variables. If these are
+not set, a pinned version will be used instead.
+"""
 
 from __future__ import annotations
 
 import os
 import shutil
-import typing
+import tarfile
+import tempfile
+import zipfile
 from pathlib import Path
 from typing import Any, Dict
 
+import requests
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from packaging.tags import sys_tags
 
 ROOT_DIR = Path(__file__).parent.resolve()
-PACT_VERSION = "2.0.7"
-PACT_URL = "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v{version}/pact-{version}-{os}-{machine}.{ext}"
-PACT_DISTRIBUTIONS: list[tuple[str, str, str]] = [
-    ("linux", "arm64", "tar.gz"),
-    ("linux", "x86_64", "tar.gz"),
-    ("osx", "arm64", "tar.gz"),
-    ("osx", "x86_64", "tar.gz"),
-    ("windows", "x86", "zip"),
-    ("windows", "x86_64", "zip"),
-]
+
+PACT_BIN_VERSION = os.getenv("PACT_BIN_VERSION", "2.0.7")
+PACT_BIN_URL = "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v{version}/pact-{version}-{os}-{machine}.{ext}"
 
 
-class PactBuildHook(BuildHookInterface):
+class UnsupportedPlatformError(RuntimeError):
+    """Raised when the current platform is not supported."""
+
+    def __init__(self, platform: str) -> None:
+        """
+        Initialize the exception.
+
+        Args:
+            platform: The unsupported platform.
+        """
+        self.platform = platform
+        super().__init__(f"Unsupported platform {platform}")
+
+
+class PactBuildHook(BuildHookInterface[Any]):
     """Custom hook to download Pact binaries."""
 
     PLUGIN_NAME = "custom"
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # noqa: ANN401
+        """
+        Initialize the build hook.
+
+        For this hook, we additionally define the lib extension based on the
+        current platform.
+        """
+        super().__init__(*args, **kwargs)
+        self.tmpdir = Path(tempfile.TemporaryDirectory().name)
+        self.tmpdir.mkdir(parents=True, exist_ok=True)
 
     def clean(self, versions: list[str]) -> None:  # noqa: ARG002
         """Clean up any files created by the build hook."""
@@ -43,10 +74,9 @@ class PactBuildHook(BuildHookInterface):
         build_data["infer_tag"] = True
         build_data["pure_python"] = False
 
-        pact_version = os.getenv("PACT_VERSION", PACT_VERSION)
-        self.install_pact_binaries(pact_version)
+        self.pact_bin_install(PACT_BIN_VERSION)
 
-    def install_pact_binaries(self, version: str) -> None:  # noqa: PLR0912
+    def pact_bin_install(self, version: str) -> None:
         """
         Install the Pact standalone binaries.
 
@@ -54,10 +84,30 @@ class PactBuildHook(BuildHookInterface):
         the current operating system is determined automatically.
 
         Args:
-            version: The Pact version to install. Defaults to the value in
-            `PACT_VERSION`.
+            version: The Pact version to install.
         """
-        platform = typing.cast(str, next(sys_tags()).platform)
+        url = self._pact_bin_url(version)
+        if url:
+            artifact = self._download(url)
+            self._pact_bin_extract(artifact)
+
+    def _pact_bin_url(self, version: str) -> str | None:
+        """
+        Generate the download URL for the Pact binaries.
+
+        Generate the download URL for the Pact binaries based on the current
+        platform and specified version. This function mainly contains a lot of
+        matching logic to determine the correct URL to use, due to the
+        inconsistencies in naming conventions between ecosystems.
+
+        Args:
+            version: The upstream Pact version.
+
+        Returns:
+            The URL to download the Pact binaries from, or None if the current
+            platform is not supported.
+        """
+        platform = next(sys_tags()).platform
 
         if platform.startswith("macosx"):
             os = "osx"
@@ -66,11 +116,15 @@ class PactBuildHook(BuildHookInterface):
             elif platform.endswith("x86_64"):
                 machine = "x86_64"
             else:
-                msg = f"Unknown macOS machine {platform}"
-                raise ValueError(msg)
-            url = PACT_URL.format(version=version, os=os, machine=machine, ext="tar.gz")
+                raise UnsupportedPlatformError(platform)
+            return PACT_BIN_URL.format(
+                version=version,
+                os=os,
+                machine=machine,
+                ext="tar.gz",
+            )
 
-        elif platform.startswith("win"):
+        if platform.startswith("win"):
             os = "windows"
 
             if platform.endswith("amd64"):
@@ -78,70 +132,78 @@ class PactBuildHook(BuildHookInterface):
             elif platform.endswith(("x86", "win32")):
                 machine = "x86"
             else:
-                msg = f"Unknown Windows machine {platform}"
-                raise ValueError(msg)
+                raise UnsupportedPlatformError(platform)
+            return PACT_BIN_URL.format(
+                version=version,
+                os=os,
+                machine=machine,
+                ext="zip",
+            )
 
-            url = PACT_URL.format(version=version, os=os, machine=machine, ext="zip")
-
-        elif "linux" in platform:
+        if "manylinux" in platform:
             os = "linux"
             if platform.endswith("x86_64"):
                 machine = "x86_64"
             elif platform.endswith("aarch64"):
                 machine = "arm64"
             else:
-                msg = f"Unknown Linux machine {platform}"
-                raise ValueError(msg)
+                raise UnsupportedPlatformError(platform)
+            return PACT_BIN_URL.format(
+                version=version,
+                os=os,
+                machine=machine,
+                ext="tar.gz",
+            )
 
-            url = PACT_URL.format(version=version, os=os, machine=machine, ext="tar.gz")
+        raise UnsupportedPlatformError(platform)
 
-        else:
-            msg = f"Unknown platform {platform}"
-            raise ValueError(msg)
-
-        self.download_and_extract_pact(url)
-
-    def download_and_extract_pact(self, url: str) -> None:
+    def _pact_bin_extract(self, artifact: Path) -> None:
         """
-        Download and extract the Pact binaries.
+        Extract the Pact binaries.
 
-        If the download artifact is already present, it will be used instead of
-        downloading it again.
+        The upstream distributables contain a lot of files which are not needed
+        for this library. This function ensures that only the files in
+        `pact/bin` are extracted to avoid unnecessary bloat.
 
         Args:
-            url: The URL to download the Pact binaries from.
+            artifact: The path to the downloaded artifact.
+        """
+        if str(artifact).endswith(".zip"):
+            with zipfile.ZipFile(artifact) as f:
+                f.extractall(ROOT_DIR)
+
+        if str(artifact).endswith(".tar.gz"):
+            with tarfile.open(artifact) as f:
+                f.extractall(ROOT_DIR)
+
+        # Cleanup the extract `README.md`
+        (ROOT_DIR / "pact" / "README.md").unlink()
+
+    def _download(self, url: str) -> Path:
+        """
+        Download the target URL.
+
+        This will download the target URL to the `pact/data` directory. If the
+        download artifact is already present, its path will be returned.
+
+        Args:
+            url: The URL to download
+
+        Return:
+            The path to the downloaded artifact.
         """
         filename = url.split("/")[-1]
         artifact = ROOT_DIR / "pact" / "data" / filename
         artifact.parent.mkdir(parents=True, exist_ok=True)
 
-        if not filename.endswith((".zip", ".tar.gz")):
-            msg = f"Unknown artifact type {filename}"
-            raise ValueError(msg)
-
         if not artifact.exists():
-            import requests
-
             response = requests.get(url, timeout=30)
-            response.raise_for_status()
+            try:
+                response.raise_for_status()
+            except requests.HTTPError as e:
+                msg = f"Failed to download from {url}."
+                raise RuntimeError(msg) from e
             with artifact.open("wb") as f:
                 f.write(response.content)
 
-        if filename.endswith(".zip"):
-            import zipfile
-
-            with zipfile.ZipFile(artifact) as f:
-                f.extractall(ROOT_DIR)
-        if filename.endswith(".tar.gz"):
-            import tarfile
-
-            with tarfile.open(artifact) as f:
-                f.extractall(ROOT_DIR)
-
-        # Move the README that is extracted from the Ruby standalone binaries to
-        # the `data` subdirectory.
-        if (ROOT_DIR / "pact" / "README.md").exists():
-            shutil.move(
-                ROOT_DIR / "pact" / "README.md",
-                ROOT_DIR / "pact" / "data" / "README.md",
-            )
+        return artifact
