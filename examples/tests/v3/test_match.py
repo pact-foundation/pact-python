1f2fb0b4b24eba13f6e79252bccb893f0c13edd6
JP-Ellis <josh@jpellis.me>
2024-10-01 17:07:04 +1000

    chore: get test to run again
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- examples/tests/v3/test_match.py
+++ examples/tests/v3/test_match.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import requests
 
 from examples.tests.v3.basic_flask_server import start_provider
-from pact.v3 import Pact, Verifier, generators, match
+from pact.v3 import Pact, Verifier, generate, match
 
 
 def test_matchers() -> None:
@@ -17,15 +17,15 @@ def test_matchers() -> None:
     (
         pact.upon_receiving("a request")
         .given("a state", parameters={"providerStateArgument": "providerStateValue"})
-        .with_request("GET", match.regex(r"/path/to/\d{1,4}", "/path/to/100"))
+        .with_request("GET", match.regex("/path/to/100", regex=r"/path/to/\d{1,4}"))
         .with_query_parameter(
             "asOf",
             match.like(
                 [
                     match.date("yyyy-MM-dd", "2024-01-01"),
                 ],
-                min_count=1,
-                max_count=1,
+                min=1,
+                max=1,
             ),
         )
         .will_respond_with(200)
@@ -33,35 +33,37 @@ def test_matchers() -> None:
             "response": match.like(
                 {
                     "regexMatches": match.regex(
-                        r".*hello world'$", "must end with 'hello world'"
+                        "must end with 'hello world'",
+                        regex=r"^.*hello world'$",
                     ),
                     "randomRegexMatches": match.regex(
                         r"1-8 digits: \d{1,8}, 1-8 random letters \w{1,8}"
                     ),
-                    "integerMatches": match.integer(42),
+                    "integerMatches": match.int(42),
                     "decimalMatches": match.decimal(3.1415),
-                    "randomIntegerMatches": match.integer(min_val=1, max_val=100),
-                    "randomDecimalMatches": match.decimal(digits=4),
-                    "booleanMatches": match.boolean(value=False),
+                    "randomIntegerMatches": match.int(min=1, max=100),
+                    "randomDecimalMatches": match.decimal(precision=4),
+                    "booleanMatches": match.bool(False),
                     "randomStringMatches": match.string(size=10),
                     "includeMatches": match.includes("world"),
                     "includeWithGeneratorMatches": match.includes(
-                        "world", generators.regex(r"\d{1,8} (hello )?world \d+")
+                        "world",
+                        generator=generate.regex(r"\d{1,8} (hello )?world \d+"),
                     ),
                     "minMaxArrayMatches": match.each_like(
-                        match.number(digits=2),
-                        min_count=3,
-                        max_count=5,
+                        match.number(1.23, precision=2),
+                        min=3,
+                        max=5,
                     ),
                     "arrayContainingMatches": match.array_containing([
-                        match.integer(1),
-                        match.integer(2),
+                        match.int(1),
+                        match.int(2),
                     ]),
                     "numbers": {
                         "intMatches": match.number(42),
                         "floatMatches": match.number(3.1415),
-                        "intGeneratorMatches": match.number(max_val=10),
-                        "decimalGeneratorMatches": match.number(digits=4),
+                        "intGeneratorMatches": match.number(2, max=10),
+                        "decimalGeneratorMatches": match.number(3.1415, precision=4),
                     },
                     "dateMatches": match.date("yyyy-MM-dd", "2024-01-01"),
                     "randomDateMatches": match.date("yyyy-MM-dd"),
@@ -79,13 +81,15 @@ def test_matchers() -> None:
                                 rules=match.string("John Doe"),
                             )
                         },
-                        rules=match.regex(r"id_\d+", "id_1"),
+                        rules=match.regex("id_1", regex=r"^id_\d+$"),
                     ),
                 },
-                min_count=1,
+                min=1,
             )
         })
-        .with_header("SpecialHeader", match.regex(r"Special: \w+", "Special: Foo"))
+        .with_header(
+            "SpecialHeader", match.regex("Special: Foo", regex=r"%Special: \w+")
+        )
     )
     with pact.serve() as mockserver:
         response = requests.get(
