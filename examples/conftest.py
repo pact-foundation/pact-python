d1d46329abdef24670e702c07b560d1f281921c4
JP-Ellis <josh@jpellis.me>
2024-12-13 12:17:24 +1100

    chore: add pytest-xdist
    Parallelise the tests so the test suite runs faster.
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- examples/conftest.py
+++ examples/conftest.py
@@ -12,6 +12,9 @@ after the examples have been run.
 
 from __future__ import annotations
 
+import socket
+import sys
+import warnings
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
@@ -20,7 +23,9 @@ from testcontainers.compose import DockerCompose  # type: ignore[import-untyped]
 from yarl import URL
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
+    from collections.abc import Generator, Sequence
+
+    import execnet
 
 EXAMPLE_DIR = Path(__file__).parent.resolve()
 
@@ -45,6 +50,13 @@ def broker(request: pytest.FixtureRequest) -> Generator[URL, Any, None]:
         yield URL(broker_url)
         return
 
+    # Check whether port 9292 is already in use. If it is, we assume that the
+    # broker is already running and return early.
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        if s.connect_ex(("localhost", 9292)) == 0:
+            yield URL("http://pactbroker:pactbroker@localhost:9292")
+            return
+
     with DockerCompose(
         EXAMPLE_DIR,
         compose_file_name=["docker-compose.yml"],
@@ -52,10 +64,32 @@ def broker(request: pytest.FixtureRequest) -> Generator[URL, Any, None]:
         wait=False,
     ) as _:
         yield URL("http://pactbroker:pactbroker@localhost:9292")
-    return
 
 
 @pytest.fixture(scope="session")
 def pact_dir() -> Path:
     """Fixture for the Pact directory."""
     return EXAMPLE_DIR / "pacts"
+
+
+def pytest_xdist_setupnodes(
+    config: pytest.Config,  # noqa: ARG001
+    specs: Sequence[execnet.XSpec],
+) -> None:
+    """
+    Hook to check if the examples are run with multiple workers.
+
+    The examples are designed to run in a specific order, with the consumer
+    tests running _before_ the provider tests as the provider tests require that
+    the consumer-generated Pacts are published.
+
+    If multiple xdist workers are detected, a warning is printed to the console.
+    """
+    if len(specs) > 1:
+        sys.stderr.write("\n")
+        warnings.warn(
+            "Running the examples with multiple workers may cause issues. "
+            "Consider running the examples with a single worker by setting "
+            "`--numprocesses=1` or using `hatch run example`.",
+            stacklevel=1,
+        )
