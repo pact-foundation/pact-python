f68e2a3640db5fbedf4259fdfdd431ceef3e7738
JP-Ellis <josh@jpellis.me>
2025-08-07 14:58:43 +1000

    fix: with metadata function signature
    The use of double-underscores was unnecessary, and did not work as
    intended. Instead, the `/` separator ensures that positional-only keys
    don't clash with explicit keyword arguments.
    
    The documentation has been adjusted to reflect this.
    
    Also, the `set_metadata` variant has been added to allow for raw values
    to be passed without further encoding/decoding.
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- tests/interaction/test_sync_message_interaction.py
+++ tests/interaction/test_sync_message_interaction.py
@@ -5,6 +5,7 @@ Pact Async Message Interaction unit tests.
 from __future__ import annotations
 
 import re
+from unittest.mock import MagicMock
 
 import pytest
 
@@ -16,7 +17,7 @@ def pact() -> Pact:
     """
     Fixture for a Pact instance.
     """
-    return Pact("consumer", "provider")
+    return Pact("consumer", "provider").with_specification("V4")
 
 
 def test_str(pact: Pact) -> None:
@@ -33,3 +34,71 @@ def test_repr(pact: Pact) -> None:
         )
         is not None
     )
+
+
+def test_with_metadata_with_positional_dict(pact: Pact) -> None:
+    (
+        pact.upon_receiving("with_metadatadict", "Sync")
+        .with_body("request", content_type="text/plain")
+        .with_metadata({"foo": "bar"})
+        .will_respond_with()
+        .with_body("response", content_type="text/plain")
+    )
+    handler = MagicMock()
+    handler.return_value = "response"
+    pact.verify(handler, "Sync")
+    handler.assert_called_once()
+    assert "foo" in handler.call_args[0][1]
+    assert handler.call_args[0][1]["foo"] == "bar"
+
+
+def test_with_metadata_with_keyword_args(pact: Pact) -> None:
+    (
+        pact.upon_receiving("with_metadata_kwargs", "Sync")
+        .with_body("request", content_type="text/plain")
+        .with_metadata(foo="bar")
+        .will_respond_with()
+        .with_body("response", content_type="text/plain")
+    )
+    handler = MagicMock()
+    handler.return_value = "response"
+    pact.verify(handler, "Sync")
+    handler.assert_called_once()
+    assert "foo" in handler.call_args[0][1]
+    assert handler.call_args[0][1]["foo"] == "bar"
+
+
+def test_with_metadata_with_mixed_args(pact: Pact) -> None:
+    (
+        pact.upon_receiving("with_metadata_mixed", "Sync")
+        .with_body("request", content_type="text/plain")
+        .with_metadata({"foo": {"bar": 1.23}}, metadata=123)
+        .will_respond_with()
+        .with_body("response", content_type="text/plain")
+    )
+    handler = MagicMock()
+    handler.return_value = "response"
+    pact.verify(handler, "Sync")
+    handler.assert_called_once()
+    assert "foo" in handler.call_args[0][1]
+    assert handler.call_args[0][1]["foo"] == {"bar": 1.23}
+    assert "metadata" in handler.call_args[0][1]
+    assert handler.call_args[0][1]["metadata"] == 123
+
+
+def test_with_metadata_with_part(pact: Pact) -> None:
+    (
+        pact.upon_receiving("with_metadata_part", "Sync")
+        .with_body("request", content_type="text/plain")
+        .will_respond_with()
+        .with_body("response", content_type="text/plain")
+        .with_metadata({"foo": {"bar": 1.23}}, "Request", metadata=123)
+    )
+    handler = MagicMock()
+    handler.return_value = "response"
+    pact.verify(handler, "Sync")
+    handler.assert_called_once()
+    assert "foo" in handler.call_args[0][1]
+    assert handler.call_args[0][1]["foo"] == {"bar": 1.23}
+    assert "metadata" in handler.call_args[0][1]
+    assert handler.call_args[0][1]["metadata"] == 123
